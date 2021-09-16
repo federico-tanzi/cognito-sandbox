101df92c0f4edfb9533f20f2919dc98f11c9a537
federicotanzi <federicotanzi@sirius.com.ar>
2021-09-16 10:20:13 -0300

    terraform

--- src/main/java/com/ftanzi/cognito/config/ParameterStoreConfig.java
+++ src/main/java/com/ftanzi/cognito/config/ParameterStoreConfig.java
@@ -17,11 +17,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * @author naveen
- *
- * @date 30-Oct-2018
- */
 @Configuration
 public class ParameterStoreConfig {
 
