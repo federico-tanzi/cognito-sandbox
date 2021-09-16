c089a48332a1b32b4b860a60a9083364cbad3ca2
facundo <facundo915@gmail.com>
2021-09-15 23:19:43 -0300

    .

--- run-backend.sh
+++ run-backend.sh
@@ -1,7 +1,7 @@
 rm -rf build
 rm -rf .gradle
 export SPRING_PROFILES_ACTIVE=local
-./scripts/wait-for-it.sh localstack:4566
-wait 15
+./scripts/wait-for-it.sh localhost:4566
+sleep 15
 export USER_POOL_ID=`sed 's/"//g' user_pool_id.txt`
 ./gradlew bootRun
